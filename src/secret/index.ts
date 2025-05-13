f7a26d681edfb1e0b407a586f5eddba86ce3b531
dirac156 <dmurairimukongya@gmail.com>
2025-05-13 11:00:55 +0200

    runned the app

--- src/secret/index.ts
+++ src/secret/index.ts
@@ -1,20 +1,27 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const IGusername: string = process.env.IGusername || "default_IGusername";
-export const IGpassword: string = process.env.IGpassword || "default_IGpassword";
+export const IGusername: string =
+  process.env.IGusername || "default_IGusername";
+export const IGpassword: string =
+  process.env.IGpassword || "default_IGpassword";
 export const Xusername: string = process.env.Xusername || "default_Xusername";
 export const Xpassword: string = process.env.Xpassword || "default_Xpassword";
 
 export const TWITTER_API_CREDENTIALS = {
   appKey: process.env.TWITTER_API_KEY || "default_TWITTER_API_KEY",
   appSecret: process.env.TWITTER_API_SECRET || "default_TWITTER_API_SECRET",
-  accessToken: process.env.TWITTER_ACCESS_TOKEN || "default TWITTER_ACCESS_TOKEN",
-  accessTokenSecret: process.env.TWITTER_ACCESS_SECRET || "default_TWITTER_ACCESS_SECRET",
-  bearerToken: process.env.TWITTER_BEARER_TOKEN || "default_TWITTER_BEARER_TOKEN",
-}
+  accessToken:
+    process.env.TWITTER_ACCESS_TOKEN || "default TWITTER_ACCESS_TOKEN",
+  accessTokenSecret:
+    process.env.TWITTER_ACCESS_SECRET || "default_TWITTER_ACCESS_SECRET",
+  bearerToken:
+    process.env.TWITTER_BEARER_TOKEN || "default_TWITTER_BEARER_TOKEN",
+};
 
+export const groqApiKey = process.env.GROQ_API_KEY;
 
+export const geminiApiKey = process.env.GEMINI_API_KEY_1;
 
 export const geminiApiKeys = [
   process.env.GEMINI_API_KEY_1 || "API_KEY_1",
